56d5066b30d666c3a85f7c7d5b0da2dacb847cd0
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-04-07 12:52:50 -0400

    server uses fresh data batplot testing function

--- R/app_server.R
+++ R/app_server.R
@@ -6,29 +6,19 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   
+  
+  
+  # regional phenology ------------------------------------------------------
+  
+  
   # Your application server logic 
-  output$map <- leaflet::renderLeaflet(make_leaflet_map())
   chosen_region <- mod_map_select_server("reg_map",
-                                       what_to_click = "shape",
-                                       fun = make_leaflet_map,
-                                       # these are arguments to make_leaflet_map
-                                       mapdata = mapselector::CERQ,
-                                       label = TRUE,
-                                       region_name = "NOM_PROV_N")
-  
-  
-  
-  rcoleo_sites_sf <- mapselector::get_rcoleo_sites_sf()
-  
-  psites <- dplyr::left_join(rcoleo_sites_sf, 
-                             dplyr::select(tableauchangementstemporels::sites_to_plot, Nom.de.la.cellule, Numero.de.reference.du.site), 
-                             by = c("site_code" = "Numero.de.reference.du.site"))
-  
-  chosen_site <- mod_map_select_server("bat_map",
-                                       what_to_click = "marker",
-                                       fun = mapselector::plot_rcoleo_sites,
-                                       rcoleo_sites_sf = psites,
-                                       site_id_col = "Nom.de.la.cellule")
+                                         what_to_click = "shape",
+                                         fun = make_leaflet_map,
+                                         # these are arguments to make_leaflet_map
+                                         mapdata = mapselector::CERQ,
+                                         label = TRUE,
+                                         region_name = "NOM_PROV_N")
   
   
   sample_data <- select_top_n_df_input(tableauchangementstemporels::data_with_region,
@@ -37,6 +27,7 @@ app_server <- function( input, output, session ) {
   gantt_data <- format_for_gantt_figure(sample_data)
   
   count_data <- format_for_count_figure(sample_data)
+  
   
   mod_modal_make_server("modal_make_ui_1", 
                         region = chosen_region,
@@ -47,14 +38,36 @@ app_server <- function( input, output, session ) {
                                  plot_both_together(chosen_region, gantt_data, count_data))
   )
   
-  bat_df_saved <- tableauchangementstemporels::common_bats_wk
+  
+  # bat sites ---------------------------------------------------------------
+  
+  rcoleo_sites_sf <- rcoleo::download_sites_sf()
+  # filter only for those sites with bat data
+  acous <- rcoleo:::query_gen("campaigns", list(type = "acoustique"))
+  
+  rcoleo_sites_bats <- dplyr::semi_join(rcoleo_sites_sf, acous, by = c("site_code" = "site.site_code"))
+  
+  chosen_site <- mod_map_select_server("bat_map",
+                                       what_to_click = "marker",
+                                       fun = mapselector::plot_rcoleo_sites,
+                                       rcoleo_sites_sf = rcoleo_sites_bats)
+  
+  mod_observation_display_server("siteobs", 
+                                 site = rcoleo_sites_bats, 
+                                 region = chosen_site)
+  
+  # bat_df_saved <- tableauchangementstemporels::common_bats_wk
   
   mod_modal_make_server("modal_make_ui_bats", 
                         region = chosen_site,
                         title_format_pattern = "Les chauves-souris de la region %s",
                         # here place all the tabs you want in your final modal! 
                         ## this can be a function which returns a reactive output (e.g. renderPlotly)
-                        tabPanel(title = "Visualization",
-                                 plot_some_bats(chosen_site, bat_df_saved))
-  )
+                        # tabPanel(title = "Visualization",
+                        #          plot_some_bats(chosen_site, bat_df_saved)),
+                        tabPanel(title = "Observations",
+                                 mod_observation_display_ui("siteobs")
+                        )
+                                 )
+
 }
--- R/utils_batplot.R
+++ R/utils_batplot.R
@@ -1,1 +1,32 @@
 
+# library(shiny)
+testapp_bat_maps <- function(){
+  ui <- fluidPage(
+    mapselector::mod_map_select_ui("bat_map"),
+    tableOutput("sel")
+  )
+  
+  server <-  function(input, output, session) {
+    
+    rcoleo_sites_sf <- rcoleo::download_sites_sf()
+    # filter only for those sites with bat data
+    acous <- rcoleo:::query_gen("campaigns", list(type = "acoustique"))
+    
+    rcoleo_sites_bats <- dplyr::semi_join(rcoleo_sites_sf, acous, by = c("site_code" = "site.site_code"))
+    
+    
+    selsite <- mapselector::mod_map_select_server("bat_map",
+                          what_to_click = "marker",
+                          fun = mapselector::plot_rcoleo_sites,
+                          rcoleo_sites_sf = rcoleo_sites_bats)
+    
+
+    ff <- reactive({get_subset_site(site = rcoleo_sites_bats,
+                              site_code_sel = selsite())})
+    
+    output$sel <- renderTable(head(ff()))
+  }
+  shinyApp(ui, server)
+}
+
+testapp_bat_maps()
