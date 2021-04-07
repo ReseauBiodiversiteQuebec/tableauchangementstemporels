a1d02d762b25f560200cb59b97b26cdc70ab7dce
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-04-07 09:38:28 -0400

    dont' need this now that we have campaigns

--- R/utils_batplot.R
+++ R/utils_batplot.R
@@ -1,13 +1,1 @@
 
-make_leaflet_batmap <- function(){
-  leaflet::leaflet(tableauchangementstemporels::sites_to_plot,
-                   options = leaflet::leafletOptions(minZoom = 6)) %>%
-    leaflet::addTiles() %>% # Affichage du fond de carte
-    leaflet::addCircleMarkers(lng = ~long_site, # Positionnement des sites avec les coordonnées long/lat
-                     lat = ~lat_site,
-                     radius = 8, # taille du cercle
-                     # popup = obs_an()$popup_info, # Ajout de fenêtres pop-up
-                     color = ~col,
-                     label = ~Nom.de.la.cellule,
-                     layerId = ~Numero.de.reference.du.site) # Nom.de.la.cellule?
-}
